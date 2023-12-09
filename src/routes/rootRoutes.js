28a449d122ab0fc717eb906f8391ad5e1dc1e460
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-12-09 23:59:38 +0700

    up swagger test

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../../Cyber.postman_collection.json" assert { type: "json" };
 import likeRoute from "./likeRoute.js";
 import reviewRoute from "./reviewRoute.js";
 import orderRoute from "./orderRoute.js";
@@ -8,5 +10,7 @@ const rootRoute = express.Router();
 rootRoute.use("/like", likeRoute);
 rootRoute.use("/review", reviewRoute);
 rootRoute.use("/order", orderRoute);
+
+rootRoute.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default rootRoute;
